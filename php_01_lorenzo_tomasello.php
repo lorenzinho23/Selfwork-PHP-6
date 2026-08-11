aef65f3f0d4d67a91bc90714ffbc5f9052069292
Lorenzo <lorenzo_tomasello@virgilio.it>
2026-08-11 17:31:37 +0200

    delete rows

--- php_01_lorenzo_tomasello.php
+++ php_01_lorenzo_tomasello.php
@@ -33,9 +33,6 @@ $words2 = [
     'fine' => '!'
 ];
 
-$words1[] = 'la';
-$words1[] = 'a';
-
 $results = "Nel " . $words1[6][3][0] . " del cammin di nostra vita " . $words1[4] . " " . $words1[6][4] . " " . $words1[6][5] . " una selva " . $words1[6][0] . ", ché la " . $words1[7] . " via era smarrita";
 
 echo $results;
